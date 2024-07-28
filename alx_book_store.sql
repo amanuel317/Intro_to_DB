b17efe7010953cd8fe5a6f1052b662af00ba9895
amanuel317 <amanuelabraham2215@gmail.com>
2024-07-28 12:12:12 +0300

    added orders

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,6 +14,10 @@ customer_name VARCHAR(215)
 email VARCHAR(215)
 address TEXT )
 
+CREATE TABLE Orders (order_id (Primary Key)
+customer_id (Foreign Key referencing Customers table)
+order_date DATE)
+
 CREATE TABLE Order_Details(orderdetailid (Primary Key)
 order_id (Foreign Key referencing Orders table)
 book_id (Foreign Key referencing Books table)
