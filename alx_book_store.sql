6eb1aea705c4ff949861f09e1fb50b68806d2502
amanuel317 <amanuelabraham2215@gmail.com>
2024-07-28 12:10:36 +0300

    Removed spaces

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store
 
-CREATE TABLE Books (book_id (Primary Key)
+CREATE TABLE Books(book_id (Primary Key)
 title VARCHAR(130)
 author_id (Foreign Key referencing Authors table)
 price DOUBLE
 publication_date DATE)
 
-CREATE TABLE Authors (author_id (Primary Key)
+CREATE TABLE Authors(author_id (Primary Key)
 author_name VARCHAR(215))
 
-CREATE TABLE Customers (customer_id (Primary Key)
+CREATE TABLE Customers(customer_id (Primary Key)
 customer_name VARCHAR(215)
 email VARCHAR(215)
 address TEXT )
