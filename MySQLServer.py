6dae583efdc50cdf350e1881aa904dd9eb1dd609
amanuel317 <amanuelabraham2215@gmail.com>
2024-07-28 15:41:41 +0300

    mysql server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,1 +1,34 @@
 import mysql.connector
+from mysql.connector import errorcode
+
+def create_database():
+    try:
+        # Connect to MySQL server
+        cnx = mysql.connector.connect(
+            user='your_username', 
+            password='your_password',
+            host='your_host'
+        )
+        cursor = cnx.cursor()
+        
+        # Create database
+        db_name = 'alx_book_store'
+        create_db_query = f"CREATE DATABASE IF NOT EXISTS {db_name}"
+        
+        cursor.execute(create_db_query)
+        print(f"Database '{db_name}' created successfully!")
+        
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        # Close the cursor and connection
+        cursor.close()
+        cnx.close()
+
+if __name__ == "__main__":
+    create_database()
